70a483027c1bd2e6ef8bff8d85346cd972f18b58
Shadrack <skiplagat8018@gmail.com>
2025-03-04 17:23:01 +0300

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Initialize the parent User class
+        self.knowledge = []  # Initialize an empty knowledge list
+
+    def learn(self, new_knowledge):
+        # Add new knowledge to the student's knowledge list
+        self.knowledge.append(new_knowledge)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Initialize the parent User class
+        # Initialize the knowledge attribute with predefined knowledge strings
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+        # Return a random element from the knowledge list
+        return random.choice(self.knowledge)
